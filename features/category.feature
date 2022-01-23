ddc5fc4f6156c00f9fcefbcd0cbb892bb07896de
Abdulsametileri <abdulsamet.ileri@ceng.deu.edu.tr>
2022-01-23 14:51:51 +0300

    Add new features

--- features/category.feature
+++ features/category.feature
@@ -2,8 +2,31 @@ Feature: Category
   As Product Owner I want to navigate to Product Detail and Favorite pages
   So that I can go see detail of a product and my favorite list
 
-  @work
   Scenario: User should navigate to Product Detail Page when he/she select to Go to Detail
     Given that User is on Product Listing Page
     When User clicks to the Go to Detail button of "Mervin Şal"
     Then User should navigate to Product Detail page of "Mervin Şal"
+
+  Scenario: User should navigate to Favorite Page when he/she clicks to Favorites link
+    Given that User is on Product Listing Page
+    Given that User clicks to the Favorite button of
+      | Peressa    |
+      | Sweatshirt |
+    When User clicks to the Favorites link
+    Then User should navigate to Favorites page
+
+  Scenario: User should see the quantity of his/her favorite products
+    Given that User is on Product Listing Page
+    When that User clicks to the Favorite button of
+      | Tesettür Dünyası |
+      | Tuva             |
+    Then User should see the quantity of his/her favorite products as "2"
+
+  @work
+  Scenario: User should be able to clear his/her all favorite products
+    Given that User is on Product Listing Page
+    Given that User clicks to the Favorite button of
+      | Tesettür Dünyası |
+      | Tuva             |
+    When User clears all favorite products
+    Then User should see the quantity of his/her favorite products as "0"
--- features/steps/category.js
+++ features/steps/category.js
@@ -2,6 +2,7 @@ const {Given, When, Then} = require("cucumber");
 const openUrl = require("../support/action/openUrl");
 const checkElementExists = require("../support/check/checkElementExists");
 const checkUrlContains = require("../support/check/checkUrlContains")
+const checkContainsText = require("../support/check/checkContainsText")
 
 Given("that User is on Product Listing Page", async function () {
     await openUrl.call(this, "http://localhost:8080/")
@@ -15,7 +16,6 @@ When(/^User clicks to the Go to Detail button of "([^"]*)"$/, async function (pr
         async (items, productName) => {
             const melvin = items
                 .find(item => item.querySelector("#title").textContent === productName)
-            console.log(melvin.dataset)
             const button = melvin.querySelector("#detail")
             const {slug} = melvin.dataset
             await button.click()
@@ -28,4 +28,41 @@ When(/^User clicks to the Go to Detail button of "([^"]*)"$/, async function (pr
 Then(/^User should navigate to Product Detail page of "([^"]*)"$/, async function (productName) {
     const not = false
     await checkUrlContains.call(this, not, `/${this.melvinSlug}`)
+});
+
+Given(/^that User clicks to the Favorite button of$/, async function (arr) {
+    const selector = ".product-list-item"
+
+    for (let [ productName ] of arr.rawTable) {
+        await this.page.$$eval(
+            selector,
+            async (items, productName) => {
+                const relatedProduct = items
+                    .find(item => item.querySelector("#title").textContent === productName)
+                const favoriteBtn = relatedProduct.querySelector("#favorite")
+                await favoriteBtn.click()
+            },
+            productName
+        )
+    }
+});
+
+When(/^User clicks to the Favorites link$/, async function () {
+    const favoriteLink = await this.page.$("#favorites-link")
+    await favoriteLink.click()
+});
+
+Then(/^User should navigate to Favorites page$/, async function () {
+    await this.page.waitForNavigation()
+    await checkUrlContains.call(this, false, "/favorites")
+});
+
+Then(/^User should see the quantity of his\/her favorite products as "([^"]*)"$/, async function (favoriteProductCount) {
+    const selector = "#favorite-count"
+    await checkContainsText.call(this, selector, false, favoriteProductCount)
+});
+
+When(/^User clears all favorite products$/, async function () {
+    const deleteAllBtn = await this.page.$("#clear-all-favorites")
+    await deleteAllBtn.click()
 });
